1c513086cb95763b7c35d0bc4a01cef3c7c915fa
sugus.grassi <sugus.grassi@gmail.com>
2021-01-01 21:19:16 -0300

    espacio

---
+++ src/components/FaceDetails/FaceDetails.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'tachyons';
+
+const FaceDeatils = ({celebrityDetails}) => {
+	return(
+		<div className='center'> 
+			<p className='white f3'> {celebrityDetails} </p>
+		</div>
+	)
+}
+
+export default FaceDeatils;
